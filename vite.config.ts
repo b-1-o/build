2f92e955b5bcd9da0cc45b6551d04e2a724b6294
b1o <erikghabuzyan6@gmail.com>
2026-09-24 20:34:06 -0700

    Use deployment-specific Vite base paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,7 @@
-import{defineConfig}from"vite";import react from"@vitejs/plugin-react";export default defineConfig({base:"/build/",plugins:[react()]});
+import{defineConfig}from"vite";
+import react from"@vitejs/plugin-react";
+
+export default defineConfig(({mode})=>({
+  base:mode==="github"?"/build/":"/",
+  plugins:[react()]
+}));
